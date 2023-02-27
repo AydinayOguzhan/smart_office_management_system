aa03986206d1d6be0ae29f14b67ae7d39a35ff5f
Oğuzhan Aydınay <aydinayoguzhan@gmail.com>
2023-02-27 16:49:59 +0300

    Cihaz kategori postgresql'e taşındı

--- data_access/cihaz_kategoriler_dal.js
+++ data_access/cihaz_kategoriler_dal.js
@@ -10,8 +10,8 @@ class CihazKategorilerDal {
             connection.connect((successResponse) => {
                 connection.query("SELECT * FROM cihaz_kategoriler where durum = 1", (err, result) => {
                     if (err) resolve(new ErrorResult(err));
-                    if (result.length <= 0) resolve(new ErrorResult(Messages.DataNotFound));
-                    const [...parcaKategorilerObj] = result;
+                    if (result.rowCount <= 0) resolve(new SuccessResult(Messages.DataNotFound));
+                    const [...parcaKategorilerObj] = result.rows;
                     resolve(new SuccessDataResult(Messages.Successful, parcaKategorilerObj));
                 });  
             },(errorResponse) =>{
@@ -25,8 +25,8 @@ class CihazKategorilerDal {
             connection.connect((successResponse) => {
                 connection.query("SELECT * FROM cihaz_kategoriler", (err, result) => {
                     if (err) resolve(new ErrorResult(err));
-                    if (result.length <= 0) resolve(new ErrorResult(Messages.DataNotFound));
-                    const [...parcaKategorilerObj] = result;
+                    if (result.rowCount <= 0) resolve(new SuccessResult(Messages.DataNotFound));
+                    const [...parcaKategorilerObj] = result.rows;
                     resolve(new SuccessDataResult(Messages.Successful, parcaKategorilerObj));
                 });  
             },(errorResponse) =>{
@@ -40,8 +40,8 @@ class CihazKategorilerDal {
             connection.connect((successResponse)=>{
                 connection.query(`select * from cihaz_kategoriler where id=${id} and durum = 1`, (err,result)=>{
                     if(err) resolve(new ErrorResult(err));
-                    if(result.length <= 0) resolve(new ErrorResult(Messages.DataNotFound));
-                    const [parcaKategorilerObj] = result;
+                    if (result.rowCount <= 0) resolve(new SuccessResult(Messages.DataNotFound));
+                    const [parcaKategorilerObj] = result.rows;
                     resolve(new SuccessDataResult(Messages.Successful,parcaKategorilerObj));
                 });
             },(errorResponse) =>{
@@ -53,10 +53,9 @@ class CihazKategorilerDal {
     add(obj) {
         return new Promise((resolve,reject)=>{
             connection.connect((successResponse)=>{
-                connection.query(`INSERT INTO cihaz_kategoriler(adi, durum) VALUES ("${obj.adi}", 1)`, (err,result)=>{
+                connection.query(`INSERT INTO cihaz_kategoriler(adi, durum) VALUES ('${obj.adi}', 1)`, (err,result)=>{
                     if(err) resolve(new ErrorResult(err));
-                    if(result.protocol41 === true) resolve(new SuccessResult(Messages.Successful));
-                    else resolve(new ErrorResult(Messages.Unsuccessful));
+                    else resolve(new SuccessResult(Messages.Successful));
                 });
             },(errorResponse)=>{
                 reject(new ErrorResult(errorResponse));
@@ -67,10 +66,9 @@ class CihazKategorilerDal {
     update(obj) {
         return new Promise((resolve,reject)=>{
             connection.connect((successResponse)=>{
-                connection.query(`UPDATE cihaz_kategoriler SET adi="${obj.adi}" WHERE id=${obj.id}`, (err,result)=>{
+                connection.query(`UPDATE cihaz_kategoriler SET adi='${obj.adi}' WHERE id=${obj.id}`, (err,result)=>{
                     if(err) resolve(new ErrorResult(err));
-                    if(result.protocol41 === true) resolve(new SuccessResult(Messages.Successful));
-                    else resolve(new ErrorResult(Messages.Unsuccessful));
+                    else resolve(new SuccessResult(Messages.Successful));
                 });
             },(errorResponse)=>{
                 reject(new ErrorResult(errorResponse));
@@ -83,8 +81,7 @@ class CihazKategorilerDal {
             connection.connect((successResponse)=>{
                 connection.query(`UPDATE cihaz_kategoriler set durum = 0 WHERE id=${id}`, (err,result)=>{
                     if(err) resolve(new ErrorResult(err));
-                    if(result.protocol41 === true) resolve(new SuccessResult(Messages.Successful));
-                    else resolve(new ErrorResult(Messages.Unsuccessful));
+                    else resolve(new SuccessResult(Messages.Successful));
                 });
             },(errorResponse)=>{
                 reject(new ErrorResult(errorResponse));
