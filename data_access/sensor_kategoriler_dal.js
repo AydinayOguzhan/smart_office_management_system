3c4b7e922ebc68cc59913aa5fce252df6da0ed89
Oğuzhan Aydınay <aydinayoguzhan@gmail.com>
2023-02-28 10:19:04 +0300

    Sensor kategori postgresql'e eklendi

--- data_access/sensor_kategoriler_dal.js
+++ data_access/sensor_kategoriler_dal.js
@@ -10,8 +10,8 @@ class SensorKategorilerDal {
             connection.connect((successResponse) => {
                 connection.query("SELECT * FROM sensor_kategoriler where durum = 1", (err, result) => {
                     if (err) resolve(new ErrorResult(err));
-                    if (result.length <= 0) resolve(new ErrorResult(Messages.DataNotFound));
-                    const [...sensorKategorilerObj] = result;
+                    if (result.rowCount <= 0) resolve(new ErrorResult(Messages.DataNotFound));
+                    const [...sensorKategorilerObj] = result.rows;
                     resolve(new SuccessDataResult(Messages.Successful, sensorKategorilerObj));
                 });  
             },(errorResponse) =>{
@@ -25,8 +25,8 @@ class SensorKategorilerDal {
             connection.connect((successResponse) => {
                 connection.query("SELECT * FROM sensor_kategoriler", (err, result) => {
                     if (err) resolve(new ErrorResult(err));
-                    if (result.length <= 0) resolve(new ErrorResult(Messages.DataNotFound));
-                    const [...sensorKategorilerObj] = result;
+                    if (result.rowCount <= 0) resolve(new ErrorResult(Messages.DataNotFound));
+                    const [...sensorKategorilerObj] = result.rows;
                     resolve(new SuccessDataResult(Messages.Successful, sensorKategorilerObj));
                 });  
             },(errorResponse) =>{
@@ -40,8 +40,8 @@ class SensorKategorilerDal {
             connection.connect((successResponse)=>{
                 connection.query(`select * from sensor_kategoriler where id=${id} and durum = 1`, (err,result)=>{
                     if(err) resolve(new ErrorResult(err));
-                    if(result.length <= 0) resolve(new ErrorResult(Messages.DataNotFound));
-                    const [sensorKategorilerObj] = result;
+                    if(result.rowCount <= 0) resolve(new ErrorResult(Messages.DataNotFound));
+                    const [sensorKategorilerObj] = result.rows;
                     resolve(new SuccessDataResult(Messages.Successful,sensorKategorilerObj));
                 });
             },(errorResponse) =>{
@@ -53,10 +53,9 @@ class SensorKategorilerDal {
     add(obj) {
         return new Promise((resolve,reject)=>{
             connection.connect((successResponse)=>{
-                connection.query(`INSERT INTO sensor_kategoriler(adi, durum) VALUES ("${obj.adi}", ${obj.durum})`, (err,result)=>{
-                    if(err) resolve(new ErrorResult(err));
-                    if(result.protocol41 === true) resolve(new SuccessResult(Messages.Successful));
-                    else resolve(new ErrorResult(Messages.Unsuccessful));
+                connection.query(`INSERT INTO sensor_kategoriler(adi, durum) VALUES ('${obj.adi}', ${obj.durum})`, (err,result)=>{
+                    if (err) resolve(new ErrorResult(err));
+                    else resolve(new SuccessResult(Messages.Successful));
                 });
             },(errorResponse)=>{
                 reject(new ErrorResult(errorResponse));
@@ -67,10 +66,9 @@ class SensorKategorilerDal {
     update(obj) {
         return new Promise((resolve,reject)=>{
             connection.connect((successResponse)=>{
-                connection.query(`UPDATE sensor_kategoriler SET adi="${obj.adi}" WHERE id=${obj.id}`, (err,result)=>{
-                    if(err) resolve(new ErrorResult(err));
-                    if(result.protocol41 === true) resolve(new SuccessResult(Messages.Successful));
-                    else resolve(new ErrorResult(Messages.Unsuccessful));
+                connection.query(`UPDATE sensor_kategoriler SET adi='${obj.adi}' WHERE id=${obj.id}`, (err,result)=>{
+                    if (err) resolve(new ErrorResult(err));
+                    else resolve(new SuccessResult(Messages.Successful));
                 });
             },(errorResponse)=>{
                 reject(new ErrorResult(errorResponse));
@@ -82,9 +80,8 @@ class SensorKategorilerDal {
         return new Promise((resolve,reject)=>{
             connection.connect((successResponse)=>{
                 connection.query(`UPDATE sensor_kategoriler set durum=0 WHERE id=${id}`, (err,result)=>{
-                    if(err) resolve(new ErrorResult(err));
-                    if(result.protocol41 === true) resolve(new SuccessResult(Messages.Successful));
-                    else resolve(new ErrorResult(Messages.Unsuccessful));
+                    if (err) resolve(new ErrorResult(err));
+                    else resolve(new SuccessResult(Messages.Successful));
                 });
             },(errorResponse)=>{
                 reject(new ErrorResult(errorResponse));
