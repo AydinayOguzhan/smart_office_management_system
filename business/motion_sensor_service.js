2e5615c16a07616033cc2013a83b08a02567c1e4
Oğuzhan Aydınay <aydinayoguzhan@gmail.com>
2023-04-21 22:24:35 +0300

    Sistemde yeni bir hareket algılandığında bu hareket web socket ile gönderiliyor

--- business/motion_sensor_service.js
+++ business/motion_sensor_service.js
@@ -2,60 +2,62 @@ const dateFormat = require("date-and-time");
 const MailAdapter = require("../core/utilities/mail/mail_adapter");
 const ValidatorAdapter = require("../core/utilities/validatorAdapter/validatorAdapter");
 const MotionSensorDal = require("../data_access/motion_sensor_dal");
-const {sendData} = require("../web_socket/notification_socket");
+const { sendData } = require("../web_socket/notification_socket");
 const SuccessResult = require("../core/utilities/results/success_result");
 const SecurityAspectHelper = require("../core/utilities/security/securityAspectHelper");
 
-class MotionSensorService{
-    constructor(){
+class MotionSensorService {
+    constructor() {
         this.dal = new MotionSensorDal();
         this.validatorAdapter = new ValidatorAdapter();
         this.mailAdapter = new MailAdapter();
         this.securityAspectHelper = new SecurityAspectHelper();
 
         this.schema = {
-            device_id:{type:"number", optional:false},
-            device_name:{type:"string", optional: false}
+            device_id: { type: "number", optional: false },
+            device_name: { type: "string", optional: false }
         }
 
     }
 
-    async addMotion(obj, token){
+    async addMotion(obj, token) {
         const securityAspectResult = await this.securityAspectHelper.help("addMotion", token);
-        if(securityAspectResult.success === false) return securityAspectResult;
-        
+        if (securityAspectResult.success === false) return securityAspectResult;
+
 
         const validatorResult = this.validatorAdapter.validate(this.schema, obj);
-        if(validatorResult !== true) return validatorResult;
+        if (validatorResult !== true) return validatorResult;
 
         let date = new Date();
         obj.timestamp = dateFormat.format(date, "YYYY-MM-DD HH:mm:ss");
 
         const mailResult = await this.mailAdapter.sendEmail("Yeni Hareket uyarısı", `${obj.device_name} isimli cihazda
          ${obj.timestamp} tarihinde yeni bir hareket algılandı`);
-        if(mailResult.success === false) return mailResult;
+        if (mailResult.success === false) return mailResult;
+
+        sendData(obj);
 
         const result = await this.dal.addMotion(obj);
         return result;
     }
 
-    async getMotionDevices(){
+    async getMotionDevices() {
         const result = await this.dal.getMotionDevices();
         return result;
     }
 
-    async getAllMotions(){
+    async getAllMotions() {
         const result = await this.dal.getAllMotions();
         return result;
     }
 
-    async getAllMotionsByDevice(...args){
-        const [,,deviceId] = args;
+    async getAllMotionsByDevice(...args) {
+        const [, , deviceId] = args;
         const result = await this.dal.getAllMotionsByDevice(deviceId);
         return result;
     }
 
-    async getAllMotionSensorStatistics(){
+    async getAllMotionSensorStatistics() {
         const result = await this.dal.getAllMotionSensorStatistics();
         return result;
     }
