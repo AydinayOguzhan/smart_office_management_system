faaff3072315b6e8d488f7a9cf87897382ff8f68
Oğuzhan Aydınay <aydinayoguzhan@gmail.com>
2023-05-08 21:06:35 +0300

    Token başarılı şekilde onaylandıysa artık kullanıcıya ait emaili de return ediyor

--- core/aspects/security_aspect.js
+++ core/aspects/security_aspect.js
@@ -2,6 +2,7 @@ const jwtAdapter = require("../utilities/security/jwt/jwt_adapter");
 const ErrorResult = require("../utilities/results/error_result");
 const SuccessResult = require("../utilities/results/success_result");
 const Messages = require("../utilities/constants/messages");
+const SuccessDataResult = require("../utilities/results/success_data_result");
 
 function securityAspect(token, canOpenOperationClaims=[]){
     let adapter = new jwtAdapter();
@@ -17,7 +18,7 @@ function securityAspect(token, canOpenOperationClaims=[]){
         let canOpenOperationClaim = canOpenOperationClaims[i].operation_claim_name;
         for (let j = 0; j < userOperationClaims.length; j++) {
             let userOperationClaim = userOperationClaims[j].name;
-            if (canOpenOperationClaim === userOperationClaim) return new SuccessResult();
+            if (canOpenOperationClaim === userOperationClaim) return new SuccessDataResult("", result.user);
         }
     }
     return new ErrorResult(Messages.AuthorizationDenied);
