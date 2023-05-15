b633e9a17ef46660b354c3bfe3e80dd21fdb3328
Oğuzhan Aydınay <aydinayoguzhan@gmail.com>
2023-05-15 22:05:35 +0300

    Sıcaklık ve nem için push notification eklendi

--- business/reading_service.js
+++ business/reading_service.js
@@ -3,6 +3,7 @@ const dateFormat = require("date-and-time");
 const ValidatorAdapter = require("../core/utilities/validatorAdapter/validatorAdapter");
 const { randomInt } = require("crypto");
 const SecurityAspectHelper = require("../core/utilities/security/securityAspectHelper");
+const { sendData } = require("../web_socket/notification_socket");
 
 class ReadingService{
     constructor(){
@@ -34,6 +35,15 @@ class ReadingService{
         let date = new Date();
         obj.timestamp = dateFormat.format(date, "YYYY-MM-DD HH:mm:ss");
         // obj.timestamp = this.randDate();
+
+        if(obj.temperature >= 40 || obj.temperature <= -2){
+            let notificationObj = {device_id: obj.device_id, device_name: obj.device_name, reading:obj.temperature};
+            sendData(notificationObj, "temperature");
+        }
+        if(obj.humidity >= 55 || obj.humidity <= 25){
+            let notificationObj = {device_id: obj.device_id, device_name: obj.device_name, reading:obj.humidity};
+            sendData(notificationObj, "humidity");
+        }
 
         const result = await this.dal.addReading(obj);
         return result;
