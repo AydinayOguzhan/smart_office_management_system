7a7511d72b2f933436b302401898e239e3e5124d
Oğuzhan Aydınay <aydinayoguzhan@gmail.com>
2023-05-18 19:41:41 +0300

    Records dosyası static dosya olarak sunulmaya başlandı

--- app.js
+++ app.js
@@ -93,7 +93,9 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
+
+app.use("/public/records", express.static(path.join(__dirname, "public", "records")))
 
 
 app.use('/notificationsSocket', notifications.Server);
